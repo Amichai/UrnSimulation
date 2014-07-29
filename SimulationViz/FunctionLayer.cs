3255eafce8083953e695d5d45d1ce3dd376337ba
amichai <amichaimlevy@gmail.com>
2014-07-29 19:15:21 -0400

    Simulation viz.

---
+++ SimulationViz/FunctionLayer.cs
@@ -0,0 +1,48 @@
+﻿using SimulationLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace SimulationViz {
+    class FunctionLayer {
+
+        public string Function { get; private  set; }
+        internal static FunctionLayer FromXml(XElement layer) {
+            
+
+            var expString = layer.Attribute("FofXY").Value;
+            FunctionLayer l = new FunctionLayer();
+            l.Function = expString;
+            return l;
+        }
+
+        public void Paint(DoubleArray canvas) {
+            var width = canvas.ArrayWidth;
+            var height = canvas.ArrayHeight;
+            var xConv = canvas.XRange / width;
+            var yConv = canvas.YRange / height;
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    double x = i  * xConv + canvas.XMin;
+                    double y = j * yConv + canvas.YMin;
+                    byte v = (byte)((int)Math.Round(this.Eval(x, y)) % 255);
+                    canvas.PixelSet(new Vector(x, y), v);
+                }
+            }
+            canvas.Draw();
+        }
+
+        public double Eval(double x, double y) {
+            var e = IronPython.Hosting.Python.CreateEngine();
+            var scope = e.CreateScope();
+            scope.SetVariable("x", x);
+            scope.SetVariable("y", y);
+            var r = e.Execute(this.Function, scope);
+            return r;
+        }
+    }
+}
---
+++ SimulationViz/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace SimulationViz {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            var xml = XElement.Load(@"Designer.xml");
+            this.canvas.ArrayWidth = int.Parse(xml.Attribute("ArrayWidth").Value);
+            this.canvas.ArrayHeight = int.Parse(xml.Attribute("ArrayHeight").Value);
+            this.canvas.XMin = double.Parse(xml.Attribute("XMin").Value);
+            this.canvas.XMax = double.Parse(xml.Attribute("XMax").Value);
+            this.canvas.YMin = double.Parse(xml.Attribute("YMin").Value);
+            this.canvas.YMax = double.Parse(xml.Attribute("YMax").Value);
+
+
+            foreach (var layer in xml.Element("Layers").Elements()) {
+                switch (layer.Name.LocalName) {
+                    case "Function":
+                        var f = FunctionLayer.FromXml(layer);
+                        f.Paint(this.canvas);
+                        break;
+
+                }
+            }
+        }
+    }
+}
